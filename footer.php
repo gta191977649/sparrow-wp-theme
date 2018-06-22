a9f3657d60740d4e6c660cabefc7409a7116167d
ヌルポ <episodes27@gmail.com>
2018-06-23 02:30:44 +1000

    fixed credit

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
     <!-- 关闭Ｃｏｎｔａｉｎｅｒ -->
     <footer>
         <div class="container">
-        &copy <a href="<?php echo get_home_url(); ?>">Project Sparrow <?php echo date("Y"); ?> </a>/ Theme by Episodes (Le Cai)
+        &copy <a href="<?php echo get_home_url(); ?>">Project Sparrow <?php echo date("Y"); ?> </a>/ Theme by Sparrow
         </div>
     </footer>
     <?php wp_footer(); ?>
