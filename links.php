5817823853d77d61592c6222f200e179699de9a7
ヌルポ <episodes27@gmail.com>
2018-06-23 13:10:53 +1000

    add icons for header

--- links.php
+++ links.php
@@ -9,8 +9,10 @@
     <div class="container">
     <div class="row pt-3">
         <div class="col-9">
-        <h2>友链</h2>
-        <hr/>
+        <div class="card mb-4">
+		<div class="card-body">
+        <h2 class="entry-title">友链</h2>
+        
         <?php 
             $bookmarks = get_bookmarks();
             if ( !empty($bookmarks) ){
@@ -20,7 +22,8 @@
                 
                 foreach ($bookmarks as $bookmark) {
                     echo '<div class="col-3" id="friend-link">';
-                    echo '<a href="' . $bookmark->link_url . '" title="' . $bookmark->link_description . '" target="_blank" >'. get_avatar($bookmark->link_notes,64) .'</a>'.'<h5 class="name">'.'<a href="' . $bookmark->link_url.'">' .$bookmark->link_name .'</a></h5>';
+                    echo '<a href="' . $bookmark->link_url . '" title="' . $bookmark->link_description . '" target="_blank" >'. get_avatar($bookmark->link_notes,64) .'</a>'.'<h3 class="friend-name">'.'<a href="' . $bookmark->link_url.'">' .$bookmark->link_name .'</a></h3>';
+                    echo '<p class="friend-description font-italic">'.$bookmark->link_description.'</p>';
                     echo '</div>';
                 }
                 ?>
@@ -30,7 +33,8 @@
             }
         ?>
         </div>
- 
+        </div>
+        </div>
 <?php get_sidebar();?>
 <?php get_footer(); ?>
     
