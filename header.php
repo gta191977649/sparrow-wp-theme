6a347fd569061f2c368a71df964f584b21745336
ヌルポ <Le.Cai-1@student.uts.edu.au>
2018-06-23 23:51:56 +1000

    fixed header issue

--- header.php
+++ header.php
@@ -1,7 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <meta charset="utf-8">
+        <meta charset="<?php bloginfo( 'charset' ); ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <link rel="profile" href="http://gmpg.org/xfn/11">
+        <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
         <title><?php echo get_bloginfo('name') ?> - <?php echo get_bloginfo( 'description' )?></title>
         <?php wp_head(); ?>
     </head>
