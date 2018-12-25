5b94ce3644e385f217c5c38c6e643f9135546c3f
gta191977649 <episodes27@gmail.com>
2018-12-26 02:40:36 +1100

    修正了footer的博客名称显示（直接不小心hard coded了）

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
     <!-- 关闭Ｃｏｎｔａｉｎｅｒ -->
     <footer>
         <div class="container">
-        &copy <a href="<?php echo get_home_url(); ?>">Project Sparrow <?php echo date("Y"); ?> </a>/ Theme by Sparrow
+        &copy <a href="<?php echo get_home_url(); ?>"><?php echo get_bloginfo( 'name' ); ?> <?php echo date("Y"); ?> </a>/ Theme by Sparrow
         </div>
     </footer>
     <?php wp_footer(); ?>
