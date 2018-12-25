09a765470c39036b50f0b21e86d9cc177030d6d6
gta191977649 <episodes27@gmail.com>
2018-12-26 03:28:11 +1100

    添加了header透明效果

--- header.php
+++ header.php
@@ -12,6 +12,9 @@
     <body>
         <nav class="navbar navbar-expand-xl navbar-dark bg-sparrow" role="navigation">
         <div class="container">
+        <!-- frosted glass effect 
+        <div class="wrap-blur"></div>
+        -->
         <a class="navbar-brand" href="<?php echo get_home_url(); ?>"><i class="fa fa-twitter" aria-hidden="true"></i> <?php echo get_bloginfo('name') ?>
         </a>
         <!-- Brand and toggle get grouped for better mobile display -->
